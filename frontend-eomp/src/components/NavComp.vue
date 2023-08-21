09e8b65e02924c5245a69745fcb76b1c798e4d6a
inamnk28 <inamnkabi1@gmail>
2023-08-21 16:25:08 +0200

    Monday Landing Page

---
+++ frontend-eomp/src/components/NavComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <img width="50" height="50" src="https://img.icons8.com/ios/50/000000/search--v1.png" alt="search--v1"/>
+    <img width="64" height="64" src="https://img.icons8.com/pastel-glyph/64/000000/user-male-circle.png" alt="user-male-circle"/>
+    <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/000000/phone.png" alt="phone"/>
+    <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/000000/shopping-bag.png" alt="shopping-bag"/>
+  </div>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+</nav>
+</template>
---
+++ frontend-eomp/src/views/HomeView.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container d-flex flex-wrap landing justify-content-center">
+    <div class="landing-pic-one">
+      <div>
+        <p class="text-white fs-3 fw-bold">Step up your scent game with sigma male perfumes.</p>
+        <button>Shop Now</button>
+      </div>
+    </div>
+    <div class="landing-pic-two">
+      <div>
+        <p class="text-white fs-3 fw-bold">Step up your scent game with sigma male perfumes.</p>
+        <button>Shop Now</button>
+      </div>
+    </div>
+  </div>
+  <div class="d-flex land">
+    <div class="crazy-about">
+      <p class=" crazy text-uppercase fs-1 fw-bold text-white">#crazyaboutwatchwisp&wardrobeco.</p>
+    </div>
+    <div class="shoe-display">
+      <div class="shoe-one"></div>
+      <div class="shoe-two"></div>
+    </div>
+  </div>
+  <div class="best-sellers">
+    <h3 class="text-uppercase fs-2 fw-bolder">best sellers</h3>
+    <div class="d-flex">
+      <button class="text-uppercase">clothing</button>
+      <button class="text-uppercase">watches</button>
+      <button class="text-uppercase">perfumes</button>
+      <button class="text-uppercase">sneakers</button>
+    </div>
+  </div>
+</template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200&display=swap');
+* {
+  font-family: montserrat;
+}
+  .landing-pic-one {
+    width: 600px;
+    height: 650px;
+    background-image: url(https://i.postimg.cc/k5VFMn4G/zara-man-800-black-perfume.jpg);
+    background-position: center;
+    background-size: cover;
+  }
+  .landing-pic-two {
+    width: 600px;
+    height: 650px;
+    background-image: url(https://i.postimg.cc/k5VFMn4G/zara-man-800-black-perfume.jpg);
+    background-position: center;
+    background-size: cover;
+  }
+  .crazy-about {
+    width: 70vw;
+    height: 600px;
+    background-image: url(https://i.postimg.cc/N0QZJNQQ/HIGH-TOP-MEN-COVER-EU.jpg);
+    background-position: center;
+    background-size: cover;
+  }
+  .crazy {
+    margin-block: 19rem;
+    text-align: center;
+  }
+  .shoe-one {
+    height: 300px;
+    width: 30vw;
+    background-image: url(https://i.postimg.cc/hG0DGySY/CS1772-AH502-HWF57-1.jpg);
+    background-position: center;
+    background-size: cover;
+  }
+  .shoe-two {
+    height: 300px;
+    width: 30vw;
+    background-image: url(https://i.postimg.cc/XqrBdSqq/430220-3196-022034-3-1052x.jpg);
+    background-position: center;
+    background-size: cover;
+  }
+</style>
+<script>
+
+</script>
