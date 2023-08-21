1bda3bcbec6f94a34e401725dcde901f9dce1a3e
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-08-21 11:52:54 +0200

    Database connected

--- backend/api/config/index.js
+++ backend/api/config/index.js
@@ -1,1 +1,23 @@
-// 
+const {createPool} = require('mysql');
+require('dotenv').config();
+
+// Data Base Connection
+const connection = createPool({
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPwd,
+    port: process.env.dbPort,
+    database: process.env.dbName,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+connection.query('SELECT 1', (error) => {
+    if (error) {
+        console.error('Error connecting to the database:', error);
+    } else {
+        console.log('Connected to the database!');
+    }
+});
+
+module.exports = connection
--- backend/api/index.js
+++ backend/api/index.js
@@ -1,1 +1,18 @@
-//
+// const {express, routes} = require('./controller') // Note the use of .js extension for ESM
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = +process.env.PORT || 5000;
+
+// static
+app.use(express.static('./static'));
+// app.use(express.urlencoded({ extended: false }),routes);
+
+// routes.get('^/$|/challenger', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './static/html/index.html'));
+// });
+
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
