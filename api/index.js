1163a7c225c5b9ff84b25423986ee410ff9f4953
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-08-25 09:31:09 +0200

    fixing cors error

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({ extended: false }), routes);
 
-app.use(cors());
+// app.use(cors());
 
 routes.get('^/$|/challenger', (req, res) => {
   res.sendFile(path.resolve(__dirname, './static/html/index.html'));
